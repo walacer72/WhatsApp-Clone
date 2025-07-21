3d5cb943c3329a12bb8a362a3f18d085563a191e
Walace Oliveira <walacer72@gmail.com>
2025-07-21 00:19:03 -0300

    correçao @mui newChat

--- src/components/newChat.js
+++ src/components/newChat.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import './newChat.css';
-import ArrowBackIcon from '@mui/material/ArrowBack';
+import {ArrowBackIcon} from '@mui/material';
 import Api from '../Api';
 
 export const NewChat = ({ chatList, user, show, setShow }) => {
